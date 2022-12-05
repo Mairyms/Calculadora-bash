fc2dcf700a7243c962d43735a3aba6a37f746c9c
Mairyms <mairymsalcedo@gmail.com>
2022-12-05 17:52:57 -0300

    se agrego la función de multiplicar

--- calculadora.sh
+++ calculadora.sh
@@ -8,6 +8,7 @@ echo "Elige una acción:"
 echo "1. Dividir"
 echo "2. Sumar"
 echo "3. Resta"
+echo "4. Multiplicación"
 read accion # 1
 
 case $accion in
@@ -16,6 +17,8 @@ case $accion in
   2)resultado=`echo $numero1 + $numero2 | bc`
   ;;
   3)resultado=`echo  $numero1 -  $numero2 | bc`
+  ;;
+  4)resultado=`echo  $numero1 *  $numero2 | bc`
   ;;
 esac
 
